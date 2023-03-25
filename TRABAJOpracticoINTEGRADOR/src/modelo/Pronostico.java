f154f7e4c620420aff090797a5ee5e1bfc9dd49d
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-25 14:16:20 -0300

    Create Pronostico.java

---
+++ TRABAJOpracticoINTEGRADOR/src/modelo/Pronostico.java
@@ -0,0 +1,57 @@
+package modelo;
+
+
+import modelo.Partido;
+import modelo.ResultadoEnum;
+
+
+public class Pronostico {
+
+	
+	private Equipo equipo;
+	private Partido partido[];
+	private ResultadoEnum resultado;
+	
+	
+	public Pronostico() {
+	}
+	public Pronostico(Equipo equipo, Partido partido[]) {
+			this.partido = partido;
+			this.equipo = equipo;
+			this.resultado = resultado;
+	}
+	
+	
+	
+	public Equipo getEquipo() {
+		return equipo;
+	}
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+	public Partido[] getPartido() {
+		return partido;
+	}
+	public void setPartido(Partido[] partido) {
+		this.partido = partido;
+	}
+	
+	
+	
+	public int apuesta( Partido partido[]) {            // Comparacion entre tiket y resultado
+		int tiketJugador = 0;                              // variable provisoria despues aca se lee la apuesta
+
+		int count=0;
+		for (int i =0; i<partido.length;i++) {
+			
+		if (partido[i].resultado()== tiketJugador) {     // comparara si el equipo1 es 3(ganador), 1(empatador) o 0(perdedor) 
+			count=count+1;
+			
+			}		
+	
+		}
+		return count;
+				
+	}
+		
+}
