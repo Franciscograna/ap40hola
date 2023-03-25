b8b6d23b9a919da6d2eb72a9df9039570179aea2
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-25 00:32:51 -0300

    Delete producto_.java

--- Clase7/producto_.java
+++
@@ -1,9 +0,0 @@
-Descuento desc1 = new DescuentoFijo();
-desc1.setValor(14.0);
-System.out.println(desc1.valorFinal(100)); // 86
-
-//añado en producto
-
-Descuento desc2 = new DescuentoPorcentaje();
-desc2.setValor(0.3);
-System.out.println(desc2.valorFinal(100)); // 70
