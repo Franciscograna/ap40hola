329116cfa563ff1f9709524ba36a0283f9f3cb3b
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-25 19:13:44 -0300

    Update Pronostico.java

--- TRABAJOpracticoINTEGRADOR/src/modelo/Pronostico.java
+++ TRABAJOpracticoINTEGRADOR/src/modelo/Pronostico.java
@@ -9,13 +9,13 @@ public class Pronostico {
 
 	
 	private Equipo equipo;
-	private Partido partido[];
+	private Partido partido;
 	private ResultadoEnum resultado;
 	
 	
 	public Pronostico() {
 	}
-	public Pronostico(Equipo equipo, Partido partido[]) {
+	public Pronostico(Equipo equipo, Partido partido, ResultadoEnum resultado) {
 			this.partido = partido;
 			this.equipo = equipo;
 			this.resultado = resultado;
@@ -29,25 +29,25 @@ public class Pronostico {
 	public void setEquipo(Equipo equipo) {
 		this.equipo = equipo;
 	}
-	public Partido[] getPartido() {
+	public Partido getPartido() {
 		return partido;
 	}
-	public void setPartido(Partido[] partido) {
+	public void setPartido(Partido partido) {
 		this.partido = partido;
 	}
 	
 	
 	
-	public int apuesta( Partido partido[]) {        	    // Comparacion entre tiket y resultado
-		int tiketJugador = 0;                              // variable provisoria despues aca se lee la apuesta
-		Partido partid = new Partido(); 		  //para poder ingresar al metodo resultado
+	public int apuesta( Partido partido) {            // Comparacion entre tiket y resultado
+		int tiketJugador = 0; 
+		Partido partid = new Partido();
 		int count=0;
-		for (int i =0; i<partido.length;i++) {
+		
 			
-			if (partido[i].resultado(partid.getGolesEquipo1(),partid.getGolesEquipo2())== tiketJugador)  {     // comparara si el equipo1 es 3(ganador), 1(empatador) o 0(perdedor) 
+		if (partido.resultado(partid.getGolesEquipo1(),partid.getGolesEquipo2())== tiketJugador) {
 			count=count+1;
 			
-			}		
+				
 	
 		}
 		return count;
