646b5f825472b916bff7b7f7a00f58927a527981
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-25 14:19:30 -0300

    Create Equipo.java

---
+++ TRABAJOpracticoINTEGRADOR/src/modelo/Equipo.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class Equipo {
+	
+	private String equipo;
+	private String descripcion;
+	
+	
+	public Equipo() {
+	//Constructor vacio	
+						}
+	
+	public Equipo(String equipo, String descripcion ) {
+		
+		this.equipo=equipo;
+		this.descripcion= descripcion;
+		
+					}
+
+	public String getEquipo() {           // 
+		return equipo;
+	}
+
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;						//area de get y set
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {   //
+		this.descripcion = descripcion;
+	}
+
+	
+
+	
+}
