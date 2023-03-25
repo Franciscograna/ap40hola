21b0514586dd657e0ac4e732bc615c5e539de484
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-25 14:18:55 -0300

    Create ResultadoEnum.java

---
+++ TRABAJOpracticoINTEGRADOR/src/modelo/ResultadoEnum.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class ResultadoEnum {
+	
+	private String ResultadoEnum;   //ganador perdedor empate
+	
+	
+	public ResultadoEnum() {
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getResultadoEnum() {
+		return ResultadoEnum;
+	}
+
+
+	public void setResultadoEnum(String resultadoEnum) {
+		ResultadoEnum = resultadoEnum;
+	}
+	
+	
+	
+
+}
