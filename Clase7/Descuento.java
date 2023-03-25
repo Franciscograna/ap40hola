e68504aab305c9c20e953436ace89fafeb934eaa
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-03-25 00:42:25 -0300

    Create Descuento.java

---
+++ Clase7/Descuento.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+public abstract class Descuento {
+private double valor;
+  
+public double getValorDesc() {
+return valor;
+}
+public void setValorDesc(double valor) {
+this.valor = valor;
+}
+public abstract double valorFinal(double valorInicial);
+
+}
+
