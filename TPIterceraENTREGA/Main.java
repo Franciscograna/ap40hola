2e5539a03d10f6bb7d2c9d759cd1a22760859ed4
Francisco Grana <126683454+Franciscograna@users.noreply.github.com>
2023-04-21 17:07:47 -0300

    Add files via upload

---
+++ TPIterceraENTREGA/Main.java
@@ -0,0 +1,67 @@
+package main;
+
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import main.ResultadoEnum;
+import main.Equipo;
+import main.Partido;
+import main.Pronostico;
+import main.Ronda;
+import main.Persona;
+
+public class Main {
+	public static void main(String[] args) {
+					
+			ArrayList<ResultadoEnum> ResultadoEnumRondaPartido = new ArrayList<ResultadoEnum>(); // clase que guarda el resultado de eq1,eq2( agregue objeto para tener mas info)
+			ArrayList<Ronda>         rondasPartido             = new ArrayList<Ronda>();  //clase que guarda la clase partido y el numero de ronda
+			   																		
+			ArrayList<Pronostico> pronostico = new ArrayList<Pronostico>(); //clase que guarda la apuesta que debera guardar la persona
+			ArrayList<Persona> personas = new ArrayList<Persona>();
+			
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				String url = "jdbc:mysql://localhost:3307/futbolapp"; //anda
+				String usuario="root";
+				String password="";
+				
+				Connection con=DriverManager.getConnection(url,usuario,password);
+				
+				if (con != null) {
+				    System.out.println("Connected to the database");
+				} else {
+				    System.out.println("Failed to connect to the database");
+				}
+				
+				
+				
+				//metodos
+				  Ronda.leerresultados(ResultadoEnumRondaPartido, rondasPartido ,con);
+				  
+				  Pronostico.leerpronostico(personas, pronostico,con);
+				  
+				  Persona.calcularPuntajes( personas,  pronostico,  rondasPartido,  ResultadoEnumRondaPartido) ;	
+				
+				
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			  				
+		}
+	}
+
---
+++ TPIterceraENTREGA/Pronostico.java
@@ -0,0 +1,126 @@
+package main;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pronostico {
+	private ResultadoEnum resultadoApuesta;
+	private Partido partido;
+	private Persona persona;
+	
+	public Pronostico () {	
+	}
+
+	public Pronostico ( ResultadoEnum resultadoApuesta, Partido partido,  Persona persona) {
+		this.resultadoApuesta=resultadoApuesta;
+		this.partido=partido;
+		this.persona = persona;
+	}
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+	public ResultadoEnum getResultadoApuesta() {
+		return resultadoApuesta;
+	}
+
+	public void setResultadoApuesta(ResultadoEnum resultadoApuesta) {
+		this.resultadoApuesta = resultadoApuesta;
+	}
+
+	public Partido getPartido() {
+		return partido;
+	}
+
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}
+
+
+	public static void leerpronostico(ArrayList<Persona> personas, ArrayList<Pronostico> pronostico, Connection con) {
+		try {
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from pronosticos");
+			int i=0;
+			while(rs.next())			
+			 { 
+				String persona        = rs.getString(1);  //leo nombre 
+				Persona personaAdd = new Persona(persona,0); // para agregar persona
+				
+				if (i==0) {
+					personaAdd.setNombre(persona); personaAdd.setPuntos(0);personas.add(personaAdd); //agrego persona
+					
+				}
+				else  { int censo =0;
+					for(int j=0; j<personas.size();j++)
+						{
+							if (personas.get(j).getNombre().equals(personaAdd.getNombre())) 	
+								{	 
+									censo=censo+1;} // marco la que esta y no la agrego 
+									}
+					if (censo==0) {personaAdd.setNombre(persona); personaAdd.setPuntos(0);personas.add(personaAdd);}// si no lo encuentro lo agrego
+					}
+							 												 
+				String resultado1a    = "EMPATE";  //leo y guardo valores
+				String resultado2a    = "EMPATE";
+			    String gana1          = rs.getString(3);
+			    String empata         = rs.getString(4);
+			    String gana2          = rs.getString(5);
+			    String equipA1          = rs.getString(2);
+			    String equipA2          = rs.getString(6);
+			    Equipo equipoA1 = new Equipo(equipA1,"");                        // armo partido
+			    Equipo equipoA2 = new Equipo(equipA2,"");						//
+			    Partido partAdd = new Partido (equipoA1,equipoA2,0 ,0);			//
+			   
+			   
+			   if (!empata.isEmpty())  {resultado1a="EMPATE";resultado2a="EMPATE";} //si no esta vacio es empate hay "x"
+			   else {
+				   	if (gana2.isEmpty()&&empata.isEmpty()) // e2 vacio gana e1
+					{resultado1a = "GANA";resultado2a = "PIERDE";}                       //		SISTEMA DE PUNTUACION
+				   	else
+					{resultado1a = "PIERDE";resultado2a = "GANA";}                      
+			   		}
+			   
+			   	//variables intermedias para crear la lista pronostico y resultadoenum(delaapuesta)
+				ResultadoEnum apuesta = new ResultadoEnum(resultado1a,resultado2a);  //armo clases y las añado a la lista
+				apuesta.setResultadoEqu1(resultado1a);//ok
+				apuesta.setResultadoEqu2(resultado2a);	   
+				Pronostico pronosticoAdd= new Pronostico(apuesta, partAdd,personaAdd); //hay 4 partidos y 8 apuestas
+				pronosticoAdd.setResultadoApuesta(apuesta);//ok 							
+				pronosticoAdd.setPartido(partAdd);// el partido ya lo tenia el real, aunque se podria formar aqui
+				pronosticoAdd.setPersona(personaAdd);
+				pronostico.add(pronosticoAdd);
+				
+			   i=i+1;
+			   
+			  }
+			   
+			} catch (SQLException e) {
+				System.out.println("error SQL...");
+				e.printStackTrace();
+			}  
+			 
+		   System.out.println("---------");
+		   System.out.println("apuestas");
+		   System.out.println("        ");
+		   for (int indice2 =0; indice2<pronostico.size();indice2++) {
+		   System.out.println(pronostico.get(indice2).getPartido().getEquipo1().getEquipo()+ " "+pronostico.get(indice2).getPartido().getEquipo2().getEquipo()+" "+pronostico.get(indice2).getPersona().getNombre()+" "+pronostico.get(indice2).getResultadoApuesta().getResultadoEqu1()+" "+pronostico.get(indice2).getResultadoApuesta().getResultadoEqu2());
+		   }
+		   System.out.println("---------");
+
+		
+	}
+}
